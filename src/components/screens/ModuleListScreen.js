1a9d55acfa79c08a66850d39a5cfe97654b75334
charlie-newton <146384118+charlie-newton@users.noreply.github.com>
2024-01-14 12:56:38 +0000

    Completed tasks from video 3 (#2)

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,18 +1,43 @@
-import { StyleSheet, Text } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Screen from '../layout/screen';
+import initialModules from '../../data/modules.js'
+
+
 
 const ModuleListScreen = () => {
   // Initialisation -------------------
+  const modules = initialModules;
   // State ----------------------------
   // Handlers -------------------------
+  const handleSelect = () => alert('Item selected');
   // View -----------------------------
   return (
     <Screen>
-      <Text>List</Text>
+      <ScrollView style={styles.container}>
+        { modules.map((module) => {
+          return(
+            <Pressable onPress={handleSelect}>
+              <View key={module.ModuleCode} style={styles.item}>
+                <Text>{module.ModuleCode} {module.ModuleName}</Text>
+              </View>
+            </Pressable>
+          );
+        })}
+      </ScrollView>
     </Screen>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  item: {
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderColor: 'lightgray'
+  },
+  text: {
+    fontSize: 16,
+  }
+});
 
 export default ModuleListScreen;
